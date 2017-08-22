995b681b90ed3023402f03155c90737f1fa35b49
Miguel Garcia <miguel@ADRIANAs-Mac-Pro.local>
2017-08-22 17:02:52 -0500

    Initial Commit

---
+++ 1-Malloc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1-Malloc
+//
+//  Created by Miguel Garcia on 8/22/17.
+//  Copyright © 2017 Miguel Garcia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
